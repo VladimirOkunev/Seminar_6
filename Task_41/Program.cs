3bb0cccd8055011ddc52047319c5f11a4793647e
Vladimir_Okunev <vova-okunev@mail.ru>
2022-11-18 20:23:37 +0900

    added Task 41

---
+++ Task_41/Program.cs
@@ -0,0 +1,46 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//-7, 567, 89, 223-> 3
+
+int [] GetArray(int size)
+{
+	int [] array = new int[size];
+	Console.WriteLine($"Enter {size} numbers of the array: ");
+	
+	for(int i=0; i<size; i++)
+	{
+		array[i] = int.Parse(Console.ReadLine() ?? "0");
+			}
+	return array;
+}
+
+void PrintArray(int [] array)
+{
+    Console.Write("The created array: ");
+	foreach(int el in array)
+	{
+		Console.Write($"{el} ");
+	}
+    Console.WriteLine();
+}
+
+int ZeroCounter(int [] array)
+{
+	int count = 0;
+	foreach(int el in array)
+	{
+		if(el == 0) count++; 
+	}
+	return count;
+}
+
+
+
+Console.Clear();
+Console.Write("Enter size of an array: ");
+int length = int.Parse(Console.ReadLine() ?? "0");
+int [] newArray = GetArray(length);
+PrintArray(newArray);
+int counter = ZeroCounter(newArray);
+Console.Write($"Numbers of zeros in the array: {counter}");
